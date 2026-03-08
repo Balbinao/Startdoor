2632490748434831a99cc46ce43718f921e485dd
Gustavo Balbino <guto9201@gmail.com>
2026-03-08 00:30:06 -0300

    feat: adicionando dependencias e criando models e enums

---
+++ backend/src/main/java/com/example/backend/model/Empresa.java
@@ -0,0 +1,131 @@
+package com.example.backend.model;
+
+import com.example.backend.model.enums.ReceitaAnual;
+import com.example.backend.model.enums.TamanhoEmpresa;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "empresa")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of ="id")
+public class Empresa implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(name = "nome_fantasia")
+    private String nomeFantasia;
+
+    @NotBlank
+    @Column(unique = true)
+    private String cnpj;
+
+    @Column(unique = true)
+    private String user;
+
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    private String senha;
+
+    @Column(name = "media_nota_geral")
+    private BigDecimal mediaNotaGeral;
+
+    @Column(columnDefinition = "TEXT")
+    private String biografia;
+
+    @Column(name = "pais_origem")
+    private String paisOrigem;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "receita_anual")
+    private ReceitaAnual receitaAnual;
+
+    @Column(name = "data_fundacao")
+    private LocalDate dataFundacao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tamanho_empresa")
+    private TamanhoEmpresa tamanhoEmpresa;
+
+    @Column(name = "estado_sede")
+    private String estadoSede;
+
+    @Column(name = "media_salarial", precision = 7, scale = 2)
+    private BigDecimal mediaSalarial;
+
+    @Column(name = "area_atuacao")
+    private String areaAtuacao;
+
+    @Column(name = "link_site")
+    private String linkSite;
+
+    @Column(name = "link_linkedin")
+    private String linkLinkedin;
+
+    @Column(name = "link_gupy")
+    private String linkGupy;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
---
+++ backend/src/main/java/com/example/backend/model/Estudante.java
@@ -0,0 +1,117 @@
+package com.example.backend.model;
+
+import com.example.backend.model.enums.ModeloTrabalho;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "estudante")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of ="id")
+public class Estudante implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(unique = true)
+    private String user;
+
+    @Column(unique = true)
+    private String email;
+
+    private String senha;
+
+    @Column(columnDefinition = "TEXT")
+    private String biografia;
+
+    @Column(name = "pais_origem")
+    private String paisOrigem;
+
+    @Column(name = "media_nota_geral")
+    private BigDecimal mediaNotaGeral;
+
+    @Column(name = "data_nascimento")
+    private LocalDate dataNascimento;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "modelo_trabalho")
+    private ModeloTrabalho modeloTrabalho;
+
+    @Column(name = "estado_atuacao")
+    private String estadoAtuacao;
+
+    @Column(name = "setor_interesse", nullable = false)
+    private String setorInteresse;
+
+    @Column(name = "habilidades_principais")
+    private String habilidadesPrincipais;
+
+    @Column(name = "link_site")
+    private String linkSite;
+
+    @Column(name = "link_linkedin")
+    private String linkLinkedin;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_ESTUDANTE"));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+}
