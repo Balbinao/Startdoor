8755a72dc240bbe0032407d12dd6c6ace7864ff9
Gustavo Balbino <guto9201@gmail.com>
2026-03-12 20:48:32 -0300

    feat: criando DTOs para alteracao de dados

---
+++ backend/src/main/java/com/example/backend/dto/AlterarEmpresa.java
@@ -0,0 +1,10 @@
+package com.example.backend.dto;
+
+import jakarta.validation.constraints.Email;
+
+public record AlterarEmpresa (
+      String nome_fantasia,
+      @Email
+      String email
+){
+}
