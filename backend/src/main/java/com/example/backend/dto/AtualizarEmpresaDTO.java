33aa79a91d1b43fa001065ea5d106c92dcd91951
Gustavo Balbino <guto9201@gmail.com>
2026-03-12 22:08:26 -0300

    refactor: renomeando arquivo

--- backend/src/main/java/com/example/backend/dto/AtualizarEmpresaDTO.java
+++ backend/src/main/java/com/example/backend/dto/AtualizarEmpresaDTO.java
@@ -2,7 +2,7 @@ package com.example.backend.dto;
 
 import jakarta.validation.constraints.Email;
 
-public record AtualizarEmpresa (
+public record AtualizarEmpresaDTO (
       String nome_fantasia,
       @Email
       String email
