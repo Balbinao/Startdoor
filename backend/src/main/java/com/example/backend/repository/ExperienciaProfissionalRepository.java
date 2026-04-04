18001001517b666d40eb3f560ba4f6588a682521
Gustavo Balbino <guto9201@gmail.com>
2026-04-03 21:36:35 -0300

    refactor: cria query nos repository

--- backend/src/main/java/com/example/backend/repository/ExperienciaProfissionalRepository.java
+++ backend/src/main/java/com/example/backend/repository/ExperienciaProfissionalRepository.java
@@ -4,5 +4,8 @@ package com.example.backend.repository;
 import com.example.backend.model.ExperienciaProfissional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ExperienciaProfissionalRepository extends JpaRepository<ExperienciaProfissional, Long> {
+    List<ExperienciaProfissional> findByEstudanteId(Long estudanteId);
 }
